f12140ad97d563ca7f8f986d0c48786b3571b769
Nikita Verhoeven <nikita.verhoeven@myparcel.nl>
2020-12-31 16:18:48 +0100

    update 1.1.3

---
+++ upload/admin/language/nl-nl/extension/shipping/myparcel_shipping.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']    = 'MyParcel Shipping';
+
+// Text
+$_['text_shipping']    = 'Shipping';
+$_['text_success']     = 'Success: You have modified MyParcel shipping!';
+$_['text_edit']        = 'Edit MyParcel Shipping';
+
+// Entry
+$_['entry_title']      = 'Title';
+$_['entry_cost']       = 'Cost';
+$_['entry_tax_class']  = 'Tax Class';
+$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Sort Order';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify MyParcel shipping!';
